4ece9266c5f44ee018e530aad8290d3fc3cc0ae8
leeminsun1205 <minhnhut12052004@gmai.com>
2025-03-27 22:31:26 +0700

    update

--- interpret_prompt.py
+++ interpret_prompt.py
@@ -48,7 +48,7 @@ if ctx.dim() == 2:
         dist = [f"{distance[m, idx].item():.4f}" for idx in idxs]
         print(f"{m+1}: {words} {dist}")
         formatted_lines.append(f"{m+1}: {' '.join(words)}")
-    
+    print("Visualize the top-k words")
     # In thêm 5 dòng liền mạch nhau
     print("\n".join(formatted_lines[:5]))
 
